6a557d19f1137a0d5e510de7933ca07bdc4ecc3b
YOGESH <162011268+yogesh1078@users.noreply.github.com>
2026-04-28 23:07:19 +0530

    Change CORS origin to production URL

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_ORIGIN ?? 'http://localhost:3001',
+    origin: 'https://word-duel-fe.vercel.app/',
     credentials: true,
   });
   app.useGlobalPipes(
